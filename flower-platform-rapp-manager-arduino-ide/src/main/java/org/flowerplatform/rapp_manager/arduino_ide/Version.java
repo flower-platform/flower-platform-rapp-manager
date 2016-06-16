8e66327dc3b8ddd8bc8fd97e357afc2a562a3231
Claudiu Matei <claudiu@shmessenger.org>
2016-06-16 13:19:23 +0300

    Version manipulation with Maven

---
+++ flower-platform-rapp-manager-arduino-ide/src/main/java/org/flowerplatform/rapp_manager/arduino_ide/Version.java
@@ -0,0 +1,11 @@
+package org.flowerplatform.rapp_manager.arduino_ide;
+
+public final class Version {
+
+	public static String VERSION = "0.0.1";
+
+	public static String BUILD_TIMESTAMP = "198001010000";
+
+	public static String VERSION_BUILD = VERSION + "-" + BUILD_TIMESTAMP;
+
+}
